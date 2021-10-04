cfbbe4c7dcb466ee542f5cec69f84099437cfdab
sota70 <sota52350@gmail.com>
2021-10-05 06:29:56 +0900

    change port number in index.ts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -54,7 +54,7 @@ http.createServer(function (req, res) {
         res.writeHead(200, { "Content-Type": "text/plain" })
         res.end()
     }
-}).listen(3000)
+}).listen(process.env.PORT || 5000)
 
 /*
  * コマンドを登録するメソッド
